0ba6f6e7b428abdff85d3cdebf01e7e6253a9161
khanV <valeriya.khan@nu.edu.kz>
2020-12-13 17:35:51 +0900

    formed uri

---
+++ app/src/main/java/com/demo/mymovies/utils/NetworkUtils.java
@@ -0,0 +1,45 @@
+package com.demo.mymovies.utils;
+
+import android.net.Uri;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkUtils {
+    private static final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+
+    private static final String PARAMS_API_KEY = "api_key";
+    private static final String PARAMS_LANG = "language";
+    private static final String PARAMS_SORT_BY = "sort_by";
+    private static final String PARAMS_PAGE = "page";
+
+    private static final String API_KEY = "37b6626ea54efbad5cd916be7a000fcf";
+    private static final String LANGUAGE_VALUE = "ru-RU";
+    private static final String SORT_BY_POPULARITY = "popularity.desc";
+    private static final String SORT_BY_TOP_RATED = "vote_average.desc";
+
+    public static final int POPULARITY = 0;
+    public static final int TOP_RATED = 1;
+
+    public static URL buildURL(int sortBy, int page){
+        URL result = null;
+        String methodOfSort;
+        if(sortBy == POPULARITY){
+            methodOfSort = SORT_BY_POPULARITY;
+        }else{
+            methodOfSort = SORT_BY_TOP_RATED;
+        }
+        Uri uri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY,API_KEY)
+                .appendQueryParameter(PARAMS_LANG,LANGUAGE_VALUE)
+                .appendQueryParameter(PARAMS_SORT_BY,methodOfSort)
+                .appendQueryParameter(PARAMS_PAGE,Integer.toString(page))
+                .build();
+        try {
+            result = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
