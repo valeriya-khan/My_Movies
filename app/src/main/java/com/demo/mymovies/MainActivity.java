b680a04037a58842e4871994861f6a4369f10522
khanV <valeriya.khan@nu.edu.kz>
2020-12-15 21:46:49 +0900

    now main activity saves movies to database

--- app/src/main/java/com/demo/mymovies/MainActivity.java
+++ app/src/main/java/com/demo/mymovies/MainActivity.java
@@ -1,15 +1,113 @@
 package com.demo.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.demo.mymovies.data.MainViewModel;
+import com.demo.mymovies.data.Movie;
+import com.demo.mymovies.utils.JSONUtils;
+import com.demo.mymovies.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Switch switchSort;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private TextView textViewTopRated;
+    private TextView textViewPopularity;
+    private MainViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        switchSort = findViewById(R.id.switchSort);
+        recyclerView = findViewById(R.id.recyclerViewPosters);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        movieAdapter = new MovieAdapter();
+        switchSort.setChecked(true);
+        recyclerView.setAdapter(movieAdapter);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethodOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Movie movie = movieAdapter.getMovies().get(position);
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                intent.putExtra("id",movie.getId());
+                startActivity(intent);
+            }
+        });
+        movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this,"End of the list",Toast.LENGTH_SHORT).show();
+            }
+        });
+        LiveData<List<Movie>> moviesFromLiveData = viewModel.getMovies();
+        moviesFromLiveData.observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                movieAdapter.setMovies(movies);
+            }
+        });
     }
-    
+
+    public void onClickSetPopularity(View view) {
+        setMethodOfSort(false);
+        switchSort.setChecked(false);
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethodOfSort(true);
+        switchSort.setChecked(true);
+    }
+
+    private void setMethodOfSort(boolean isTopRated){
+        int methodOfSort;
+        if(isTopRated){
+            textViewTopRated.setTextColor(getResources().getColor(R.color.teal_200));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.white));
+            methodOfSort = NetworkUtils.TOP_RATED;
+        }else {
+            textViewPopularity.setTextColor(getResources().getColor(R.color.teal_200));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white));
+            methodOfSort = NetworkUtils.POPULARITY;
+        }
+        downloadData(methodOfSort,1);
+    }
+
+    private void downloadData(int methodOfSort,int page){
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort,1);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        if(movies!=null && !movies.isEmpty()){
+            viewModel.deleteAllMovies();
+            for(Movie movie:movies){
+                viewModel.insertMovie(movie);
+            }
+        }
+    }
 }
