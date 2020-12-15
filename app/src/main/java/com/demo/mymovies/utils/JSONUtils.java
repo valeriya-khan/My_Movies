52870cd927ee4d847e28911b7cf9586ceee689da
khanV <valeriya.khan@nu.edu.kz>
2020-12-15 21:42:28 +0900

    created class for extraction JSON objects

--- app/src/main/java/com/demo/mymovies/utils/JSONUtils.java
+++ app/src/main/java/com/demo/mymovies/utils/JSONUtils.java
@@ -1,4 +1,54 @@
 package com.demo.mymovies.utils;
 
+import com.demo.mymovies.data.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JSONUtils {
+    private static final String KEY_RESULTS = "results";
+    private static final String KEY_VOTE_COUNT = "vote_count";
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_ORIGINAL_TITLE = "original_title";
+    private static final String KEY_POSTER_PATH = "poster_path";
+    private static final String KEY_OVERVIEW = "overview";
+    private static final String KEY_BACKDROP_PATH = "backdrop_path";
+    private static final String KEY_VOTE_AVERAGE = "vote_average";
+    private static final String KEY_RELEASE_DATE = "release_date";
+
+    public static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/";
+    public static final String SMALL_POSTER_SIZE = "w185";
+    public static final String BIG_POSTER_SIZE = "w780";
+
+    public static ArrayList<Movie> getMoviesFromJSON(JSONObject jsonObject) {
+        ArrayList<Movie> result = new ArrayList<>();
+        if (jsonObject == null) {
+            return result;
+        }
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject objectMovie = jsonArray.getJSONObject(i);
+                int id = objectMovie.getInt(KEY_ID);
+                int voteCount = objectMovie.getInt(KEY_VOTE_COUNT);
+                String title = objectMovie.getString(KEY_TITLE);
+                String originalTitle = objectMovie.getString(KEY_ORIGINAL_TITLE);
+                String overview = objectMovie.getString(KEY_OVERVIEW);
+                String posterPath = BASE_POSTER_URL + SMALL_POSTER_SIZE + objectMovie.getString(KEY_POSTER_PATH);
+                String bigPosterPath = BASE_POSTER_URL + BIG_POSTER_SIZE + objectMovie.getString(KEY_POSTER_PATH);
+                String backdropPath = objectMovie.getString(KEY_BACKDROP_PATH);
+                double voteAverage = objectMovie.getDouble(KEY_VOTE_AVERAGE);
+                String releaseDate = objectMovie.getString(KEY_RELEASE_DATE);
+                Movie movie = new Movie(id, voteCount, title, originalTitle, overview, posterPath, bigPosterPath, backdropPath, voteAverage, releaseDate);
+                result.add(movie);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
