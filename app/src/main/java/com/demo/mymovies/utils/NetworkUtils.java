4c2795c745e1700106a85e9f035d40a97d7d552c
khanV <valeriya.khan@nu.edu.kz>
2020-12-17 00:17:30 +0900

    added methods for building URL to trailers and reviews

--- app/src/main/java/com/demo/mymovies/utils/NetworkUtils.java
+++ app/src/main/java/com/demo/mymovies/utils/NetworkUtils.java
@@ -1,7 +1,14 @@
 package com.demo.mymovies.utils;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.print.PageRange;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,21 +25,49 @@ import java.util.concurrent.ExecutionException;
 
 public class NetworkUtils {
     private static final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
-
+    private static final String BASE_VIDEO_URL = "https://api.themoviedb.org/3/movie/%S/videos";
+    private static final String BASE_REVIEW_URL = "https://api.themoviedb.org/3/movie/%s/reviews";
     private static final String PARAMS_API_KEY = "api_key";
     private static final String PARAMS_LANG = "language";
     private static final String PARAMS_SORT_BY = "sort_by";
     private static final String PARAMS_PAGE = "page";
+    private static final String PARAMS_MIN_VOTE_COUNT = "vote_count.gte";
 
     private static final String API_KEY = "37b6626ea54efbad5cd916be7a000fcf";
     private static final String LANGUAGE_VALUE = "ru-RU";
     private static final String SORT_BY_POPULARITY = "popularity.desc";
     private static final String SORT_BY_TOP_RATED = "vote_average.desc";
+    private static final String MIN_VOTE_COUNT = "1000";
 
     public static final int POPULARITY = 0;
     public static final int TOP_RATED = 1;
 
-    private static URL buildURL(int sortBy, int page){
+    public static URL buildURLToVideos(int id){
+        Uri uri = Uri.parse(String.format(BASE_VIDEO_URL,id)).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY,API_KEY)
+                .appendQueryParameter(PARAMS_LANG,LANGUAGE_VALUE)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static URL buildURLToReviews(int id){
+        Uri uri = Uri.parse(String.format(BASE_REVIEW_URL,id)).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY,API_KEY)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static URL buildURL(int sortBy, int page){
         URL result = null;
         String methodOfSort;
         if(sortBy == POPULARITY){
@@ -45,6 +80,7 @@ public class NetworkUtils {
                 .appendQueryParameter(PARAMS_LANG,LANGUAGE_VALUE)
                 .appendQueryParameter(PARAMS_SORT_BY,methodOfSort)
                 .appendQueryParameter(PARAMS_PAGE,Integer.toString(page))
+                .appendQueryParameter(PARAMS_MIN_VOTE_COUNT,MIN_VOTE_COUNT)
                 .build();
         try {
             result = new URL(uri.toString());
@@ -54,9 +90,9 @@ public class NetworkUtils {
         return result;
     }
 
-    public static JSONObject getJSONFromNetwork(int sortBy, int page){
+    public static JSONObject getJSONForReviews(int id){
         JSONObject result = null;
-        URL url = buildURL(sortBy,page);
+        URL url = buildURLToReviews(id);
         try {
             result = new JSONLoadTask().execute(url).get();
         } catch (ExecutionException e) {
@@ -65,6 +101,91 @@ public class NetworkUtils {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public static JSONObject getJSONForVideos(int id){
+        JSONObject result = null;
+        URL url = buildURLToVideos(id);
+        try {
+            result = new JSONLoadTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+//    public static JSONObject getJSONFromNetwork(int sortBy, int page){
+//        JSONObject result = null;
+//        URL url = buildURL(sortBy,page);
+//        try {
+//            result = new JSONLoadTask().execute(url).get();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
+
+    public static class JSONLoader extends AsyncTaskLoader<JSONObject>{
+        private Bundle bundle;
+
+        public JSONLoader(@NonNull Context context,Bundle bundle) {
+            super(context);
+            this.bundle = bundle;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+            forceLoad();
+        }
+
+        @Nullable
+        @Override
+        public JSONObject loadInBackground() {
+            if(bundle==null){
+                return null;
+            }
+            String urlAsString = bundle.getString("url");
+            URL url = null;
+            try {
+                url = new URL(urlAsString);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            JSONObject result = null;
+            if(url==null){
+                return null;
+            }
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder builder = new StringBuilder();
+                String line = bufferedReader.readLine();
+                while (line!=null){
+                    builder.append(line);
+                    line = bufferedReader.readLine();
+                }
+                try {
+                    result = new JSONObject(builder.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection!=null){
+                    connection.disconnect();
+                }
+            }
+            return result;
+        }
     }
 
     private static class JSONLoadTask extends AsyncTask<URL,Void, JSONObject>{
